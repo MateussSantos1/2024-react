2bf68ded6288a94fea7ce25e9c2b87bb194b4000
Mateus Santos <115822261+MateussSantos1@users.noreply.github.com>
2024-03-16 11:33:19 -0300

    slaaa

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ const App = () => {
   return (
     <div>
       <h1>Bem vindo ao mundo React</h1>
-      <button>eu sou um botão html</button>
+      <button>  OMG um botão html !!!</button>
+      <button>Outro botão HTML</button>
     </div>
   );
 }
